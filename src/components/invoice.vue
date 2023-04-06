f654088a1a6faf6b1511f7c5026528c441702082
Lydia Ego <egolydia97@gmail.com>
2023-04-06 14:02:25 +0100

    Feat: invoice component template

---
+++ src/components/invoice.vue
@@ -0,0 +1,23 @@
+<template>
+    <router-link class="invoice flex" :to="{
+        name: 'Invoice', params: { invoiceId: invoice.docId }
+    }">
+        <div class="left flex">
+            <span class="tracking-number">#{{ shortenId(invoice.docId, 6) }}</span>
+            <span class="due-date">{{ invoice.paymentDueDate }}</span>
+            <span class="person">{{ invoice.clientName }}</span>
+        </div>
+        <div class="right flex">
+            <span class="price">${{ invoice.invoiceTotal }}</span>
+            <div class="status-button flex"
+                :class="{ paid: invoice.invoicePaid, draft: invoice.invoiceDraft, pending: invoice.invoicePending }">
+                <span v-if="invoice.invoicePaid">Paid</span>
+                <span v-if="invoice.invoiceDraft">Draft</span>
+                <span v-if="invoice.invoicePending">Pending</span>
+            </div>
+            <div class="icon">
+                <img src="src\assets\icon-arrow-right.svg" alt="">
+            </div>
+        </div>
+    </router-link>
+</template>
