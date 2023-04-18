a1c75de6f0b166c492e9fa836b79bf9003779583
Lydia Ego <egolydia97@gmail.com>
2023-04-18 09:38:36 +0100

    delete codes

--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
 import router from './router'
-// import firebase from 'firebase/app'
-// import 'firebase/auth'
 import App from './App.vue'
-
-// let app;
-// firebase.auth().onAuthStateChanged(() => {
-//     if(!app){}
-// })
 
 createApp(App).use(createPinia()).use(router).mount("#app");
